1e6acc211fba14d056ae18a5423ff6509b4ee450
IRjSI <rajusuthar0077@gmail.com>
2025-04-29 11:17:40 +0530

    Add expected solution example in Challenge 5 and remove unused Challenge component

--- src/challenges/five.tsx
+++ src/challenges/five.tsx
@@ -1,4 +1,30 @@
 const solution = () => {
+    /* expected solution
+        function App() {
+            const [fruit, setFruit] = React.useState('');
+            const [fruits, setFruits] = React.useState([]);
+
+            const addFruit = () => {
+                setFruits([...fruits, fruit]);
+                setFruit('');
+            };
+
+            return (
+                <div>
+                <input
+                    type="text"
+                    placeholder="type..."
+                    value={fruit}
+                    onChange={(e) => setFruit(e.target.value)}
+                />
+                <button type="submit" onClick={addFruit}>
+                    Add
+                </button>
+            );
+        }
+
+    */
+
     
 }
 
