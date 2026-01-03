5436a06507f1a60f83854d8c0190698a2f226eec
IRjSI <rajusuthar0077@gmail.com>
2026-01-03 12:24:05 +0530

    feat: Add responsive layout to Home component; adjust PanelGroup direction based on screen size

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -240,6 +240,17 @@ const Home = () => {
       .then(res => setSolutions(res.data.data.solutions));
   }, [token, ques]);
 
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   if (!isLoggedIn) return <LandingPage />;
 
   return (
@@ -265,7 +276,7 @@ const Home = () => {
         setQues={setQues}
       />
 
-      <PanelGroup direction="horizontal">
+      <PanelGroup direction={isMobile ? "vertical" : "horizontal"}>
         <EditorPanel
           code={code}
           setCode={setCode}
